2d4eea0cf67d67b2af7a1065500203ce42e545b1
Ramón Casero <rcasero@gmail.com>
2017-09-21 16:25:47 +0100

    add classifiers and some other metainformation to the package

--- setup.py
+++ setup.py
@@ -4,16 +4,35 @@ from setuptools import setup, find_packages
 
 setup(
     name='pysto',
-    version='1.1.1',
+    version='1.1.2',
     packages=find_packages(),
 
     python_requires='>=3.6',
     install_requires=['matplotlib>=2.0','numpy>=1.13','opencv-python>=3.3.0'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'dev': ['spyder', 'twine', 'wheel', 'setuptools'],
+        'test': ['pytest'],
+    },
     
     description='Miscellaneous image processing functions',
     url='https://github.com/rcasero/pysto',
-    download_url = 'https://github.com/rcasero/pysto/archive/1.1.1.tar.gz',
+    download_url = 'https://github.com/rcasero/pysto/archive/1.1.2.tar.gz',
     author='Ramón Casero',
     author_email='rcasero@gmail.com',
     license='GPL v3',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='image processing bioinformatics',
 )
