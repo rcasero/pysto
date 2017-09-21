a275aa06c0db36648d69bf490e548cf8dcd1be2a
Ramón Casero <rcasero@gmail.com>
2017-09-21 17:55:04 +0100

    increase minor version number

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='pysto',
-    version='1.1.2',
-    download_url = 'https://github.com/rcasero/pysto/archive/1.1.2.tar.gz',
+    version='1.1.3',
+    download_url = 'https://github.com/rcasero/pysto/archive/1.1.3.tar.gz',
     packages=find_packages(),
     python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*',
     install_requires=[
