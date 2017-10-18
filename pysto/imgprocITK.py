7198470e86e0c7174fca1e2f6d24aa2fc2e2c670
Ramón Casero <rcasero@gmail.com>
2017-10-18 18:19:32 +0100

    broken code, start adding code for imshow for ITK Images

---
+++ pysto/imgprocITK.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 18 15:50:02 2017
+
+@author: rcasero
+"""
+
+"""
+@file: pysto/pysto/imgprocITK.py
+@package: pysto
+@author: Ramón Casero <rcasero@gmail.com>
+@copyright: © 2017  Ramón Casero <rcasero@gmail.com>
+@license: GPL v3
+@version: 1.0.0
+
+This file is part of pysto.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details. The offer of this
+program under the terms of the License is subject to the License
+being interpreted in accordance with English Law and subject to any
+action against the University of Oxford being under the jurisdiction
+of the English Courts.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see
+<http://www.gnu.org/licenses/>.
+"""
+
+###############################################################################
+## Summary of functions in this module:
+##
+##   imshow:
+##      matplotlib.imshow extended for the ITK Image class.
+##
+###############################################################################
+
+import SimpleITK as sitk
+import matplotlib as plt
+
+###############################################################################
+## block_split
+###############################################################################
+
+def imshow(im, **kwargs):
+    """matplotlib.imshow extended for the ITK Image class.
+    
+    Light wrapper around matplotlib.imshow so that ITK images are plotted with
+    real world coordinates.
+    
+        ... = imshow(im, ...)
+    
+    Args:
+        im: 2D ITK Image class.
+        
+        ...: Optional arguments, the same that can be passed to matplotlib.imshow.
+        
+    Returns:
+        Same as matplotlib.imshow.
+    """
+
+    # compute extent of the image
+    origin = im.GetOrigin()
+    spacing = im.GetSpacing()
+    size = im.GetSize()
+    extent = (
+            origin[0], origin[0] + (size[0]-1) * spacing[0],
+            origin[1], origin[1] + (size[1]-1) * spacing[1]
+            )
+    
+    # pass the call to matplotlib.imshow
+    return plt.imshow(sitk.GetArrayFromImage(im), extent=extent, *kwargs)
---
+++ tests/test_imshow.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 18 15:31:17 2017
+
+@author: rcasero
+"""
+
+import os
+import SimpleITK as sitk
+import pysto.imgprocITK as pitk
+
+
+# root and test data directories for pysto
+root_path = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
+data_path = os.path.join(root_path, 'tests', 'data')
+
+# image file
+im_file = os.path.join(data_path, 'euxassay_003820_14.jpg')
+
+# read image
+im = sitk.ReadImage(im_file)
+
+# give image spacing and offset values
+im.SetSpacing((14.0e-6, 14.0e-6))
+im.SetOrigin((10.0, 5.0))
+
+# plot image
+pitk.imshow(im)
