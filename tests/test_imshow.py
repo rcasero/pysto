24671809515510219d10989401c511370494d665
Ramón Casero <rcasero@gmail.com>
2017-10-23 12:42:56 +0100

    add header and put test into function

--- tests/test_imshow.py
+++ tests/test_imshow.py
@@ -1,9 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct 18 15:31:17 2017
+@file: pysto/tests/test_imfuse.py
+@package: pysto
+@author: Ramón Casero <rcasero@gmail.com>
+@copyright: © 2017  Ramón Casero <rcasero@gmail.com>
+@license: GPL v3
+@version: 0.1.0
 
-@author: rcasero
+This file is part of pysto.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details. The offer of this
+program under the terms of the License is subject to the License
+being interpreted in accordance with English Law and subject to any
+action against the University of Oxford being under the jurisdiction
+of the English Courts.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see
+<http://www.gnu.org/licenses/>.
 """
 
 import os
@@ -14,15 +37,17 @@ import pysto.imgprocITK as pitk
 root_path = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
 data_path = os.path.join(root_path, 'tests', 'data')
 
-# image file
-im_file = os.path.join(data_path, 'euxassay_003820_14.jpg')
-
-# read image
-im = sitk.ReadImage(im_file)
-
-# give image spacing and offset values
-im.SetSpacing((14.0e-6, 14.0e-6))
-im.SetOrigin((10.0, 5.0))
-
-# plot image
-pitk.imshow(im)
+def test_imshow():
+    
+    # image file
+    im_file = os.path.join(data_path, 'euxassay_003820_14.jpg')
+    
+    # read image
+    im = sitk.ReadImage(im_file)
+    
+    # give image spacing and offset values
+    im.SetSpacing((14.0e-6, 14.0e-6))
+    im.SetOrigin((10.0, 5.0))
+    
+    # plot image
+    pitk.imshow(im)
